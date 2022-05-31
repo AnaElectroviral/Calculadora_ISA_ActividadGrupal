c0eb0d8d367d9419d75b985c2c0bafd946b508c9
Alain-Basabe <a.basabe@lookiero.com>
2022-05-31 11:17:17 +0200

    Definicion del test

--- Calculadora/src/test/java/CalculadoraTest.java
+++ Calculadora/src/test/java/CalculadoraTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
+    @Test
+    public deberia_sumar_numeros(){
+        //given
+        // la calculadora y datos de los numeros
+        //when
+        // sumo los numeros
+        // then
+        // el resultado deberia ser el esperado.
+    }
 
 }
