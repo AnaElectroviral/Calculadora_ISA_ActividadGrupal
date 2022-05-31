88501717504694e241111598d9160d095ede4ffe
Alain-Basabe <a.basabe@lookiero.com>
2022-05-31 14:58:17 +0200

    Definicion del test raiz

--- Calculadora/src/test/java/CalculadoraTest.java
+++ Calculadora/src/test/java/CalculadoraTest.java
@@ -65,4 +65,27 @@ class CalculadoraTest {
         final double expected = numero1 / numero2;
         assertEquals(expected,resultado);
     }
+    @Test
+    public void deberia_hacer_la_raiz_cuadrada_de_un_numero(){
+        //given
+        // la calculadora y datos de los numeros
+        final double numero1=3;
+        final double indice=2;
+        double t;
+        final Calculadora calculadora = new Calculadora();
+        //when
+        // sumo los numeros
+        final double resultado = calculadora.raiz(numero1);
+        // then
+        // el resultado deberia ser el esperado.
+        double square = calculadora.dividir(numero1,indice);
+
+        do {
+            t = square;
+            square = calculadora.dividir((calculadora.sumar(t, (calculadora.dividir(numero1, t)))),indice);
+
+        } while ((calculadora.restar(t,square)) != 0 );
+        final double expected = square;
+        assertEquals(expected,resultado);
+    }
 }
