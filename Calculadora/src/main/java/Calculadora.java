e535cabf348a2a8a57cee3eb27751c5ab8c2511b
Alain-Basabe <a.basabe@lookiero.com>
2022-05-31 11:27:09 +0200

    Primera ejecucion del test suma en rojo

--- Calculadora/src/main/java/Calculadora.java
+++ Calculadora/src/main/java/Calculadora.java
@@ -1,2 +1,13 @@
 public class Calculadora {
+<<<<<<< HEAD
 }
+=======
+    public Calculadora(){
+
+    }
+
+    public double sumar(double numero1, double numero2) {
+        return 0;
+    }
+}
+>>>>>>> 98d67b6 (Primera ejecucion del test suma en rojo)
