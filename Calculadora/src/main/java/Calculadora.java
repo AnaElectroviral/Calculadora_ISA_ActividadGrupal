f3158d64d2fc01cd30d6bfc9e27e4a8ca0b9b88c
Alain-Basabe <a.basabe@lookiero.com>
2022-05-31 11:31:51 +0200

    Metodo suma terminado con resultado test correcto

--- Calculadora/src/main/java/Calculadora.java
+++ Calculadora/src/main/java/Calculadora.java
@@ -7,7 +7,7 @@ public class Calculadora {
     }
 
     public double sumar(double numero1, double numero2) {
-        return 0;
+        return numero1 + numero2;
     }
 }
 >>>>>>> 98d67b6 (Primera ejecucion del test suma en rojo)
