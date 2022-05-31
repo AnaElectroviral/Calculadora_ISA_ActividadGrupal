3e4a6b93c7b152b03db58ab32ef995e8959adebd
Alain-Basabe <a.basabe@lookiero.com>
2022-05-31 11:46:31 +0200

    Definicion del test multiplicar

--- Calculadora/src/test/java/CalculadoraTest.java
+++ Calculadora/src/test/java/CalculadoraTest.java
@@ -33,7 +33,22 @@ class CalculadoraTest {
         // el resultado deberia ser el esperado.
         final double expected = numero1 - numero2;
         assertEquals(expected,resultado);
+    }
 
+    @Test
+    public void deberia_multiplicar_numeros(){
+        //given
+        // la calculadora y datos de los numeros
+        final double numero1=3;
+        final double numero2=5;
+        final Calculadora calculadora = new Calculadora();
+        //when
+        // sumo los numeros
+        final double resultado = calculadora.multiplicar(numero1,numero2);
+        // then
+        // el resultado deberia ser el esperado.
+        final double expected = numero1 * numero2;
+        assertEquals(expected,resultado);
     }
 
 }
