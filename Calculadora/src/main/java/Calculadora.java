1cfa3fa353f485dc2a2dfee1871f8d36f33203ae
Alain-Basabe <a.basabe@lookiero.com>
2022-05-31 11:48:07 +0200

    Primera ejecucion del test multiplicar en rojo

--- Calculadora/src/main/java/Calculadora.java
+++ Calculadora/src/main/java/Calculadora.java
@@ -10,4 +10,8 @@ public class Calculadora {
     public double restar(double numero1, double numero2) {
         return numero1 - numero2;
     }
+
+    public double multiplicar(double numero1, double numero2) {
+        return 0;
+    }
 }
