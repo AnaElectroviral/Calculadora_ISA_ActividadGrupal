98d67b607ff290946c4e5bfe0c64a14be1dce0e7
Alain-Basabe <a.basabe@lookiero.com>
2022-05-31 11:27:09 +0200

    Primera ejecucion del test suma en rojo

--- Calculadora/src/test/java/CalculadoraTest.java
+++ Calculadora/src/test/java/CalculadoraTest.java
@@ -4,13 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
     @Test
-    public deberia_sumar_numeros(){
+    public void deberia_sumar_numeros(){
         //given
         // la calculadora y datos de los numeros
+        final double numero1=3;
+        final double numero2=5;
+        final Calculadora calculadora = new Calculadora();
         //when
         // sumo los numeros
+        final double resultado = calculadora.sumar(numero1,numero2);
         // then
         // el resultado deberia ser el esperado.
+        final double expected = numero1 + numero2;
+        assertEquals(expected,resultado);
+
     }
 
 }
