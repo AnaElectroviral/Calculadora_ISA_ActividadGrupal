7b9073ce3a5887da69bde91f4a37c049014e6920
Alain-Basabe <a.basabe@lookiero.com>
2022-06-01 11:11:06 +0200

    Primera ejecucion del test resta en rojo

--- Calculadora/src/main/java/Calculadora.java
+++ Calculadora/src/main/java/Calculadora.java
@@ -1,7 +1,4 @@
 public class Calculadora {
-<<<<<<< HEAD
-}
-=======
     public Calculadora(){
 
     }
@@ -9,5 +6,8 @@ public class Calculadora {
     public double sumar(double numero1, double numero2) {
         return numero1 + numero2;
     }
+
+    public double restar(double numero1, double numero2) {
+        return 0;
+    }
 }
->>>>>>> 98d67b6 (Primera ejecucion del test suma en rojo)
